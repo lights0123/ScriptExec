b930f036303dc107bd3d6fbf63f06faee6d5d832
lights0123 <me@lights0123.com>
2014-08-13 07:38:20 -0400

    require-vault option

--- src/main/java/com/lights0123/ScriptExec/ScriptExec.java
+++ src/main/java/com/lights0123/ScriptExec/ScriptExec.java
@@ -7,16 +7,22 @@ public final class ScriptExec extends JavaPlugin{
 	public static Permission perms;
 	@Override
 	public void onEnable(){
+		this.saveDefaultConfig();
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 		if(!setupPermissions()){
-			getLogger().severe("Vault not found. All players will have permission to execute scripts!");
+			if(getConfig().getBoolean("require-vault")){
+				getLogger().severe("Vault not found. Shutting down...");
+				getServer().getPluginManager().disablePlugin(this);
+	            return;
+			}else{
+				getLogger().severe("Vault not found. All players will have permission to execute scripts!");
+			}
 			hasPerms=false;
 		}else{
 			hasPerms=true;
 		}
 		this.getCommand("se").setExecutor(new SeCommand(this));
-		this.saveDefaultConfig();
-		getConfig().options().copyDefaults(true);
-		saveConfig();
 	}
 	public void onDisable(){
 		String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion());
