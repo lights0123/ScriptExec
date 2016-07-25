bf4b0d6ddeb98cecbc929f290b48d950b91363ff
lights0123 <me@lights0123.com>
2016-07-25 11:52:31 -0400

    Cleanup code

--- src/main/java/com/lights0123/ScriptExec/CommandExecutor.java
+++ src/main/java/com/lights0123/ScriptExec/CommandExecutor.java
@@ -1,4 +1,5 @@
 package com.lights0123.ScriptExec;
+
 import java.io.IOException;
 
 import org.bukkit.ChatColor;
@@ -6,49 +7,50 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CommandExecutor implements org.bukkit.command.CommandExecutor{
+class CommandExecutor implements org.bukkit.command.CommandExecutor {
 	private final ScriptExec plugin;
-	 
-	public CommandExecutor(ScriptExec plugin) {
+
+	CommandExecutor(ScriptExec plugin) {
 		this.plugin = plugin;
 	}
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("scriptexec")) {
-			if(args.length==0){
-				if(perm(sender,"ScriptExec.info")){
-					sender.sendMessage(ChatColor.GOLD+"ScriptExec - a plugin by lights0123 to execute\nconsole commands on the server by in-game commands.\nView more information at "+ChatColor.BLUE+ChatColor.UNDERLINE+"http://dev.bukkit.org/bukkit-plugins/scriptexec"+ChatColor.GOLD+"\n/se reload: reload the configuration.\n/se execute <name>: execute a script.");
-				}else{
-					sender.sendMessage(ChatColor.RED+"Sorry, but you do not have permission!");
+			if (args.length == 0) {
+				if (perm(sender, "ScriptExec.info")) {
+					sender.sendMessage(ChatColor.GOLD + "ScriptExec - a plugin by lights0123 to execute\nconsole commands on the server by in-game commands.\nView more information at " + ChatColor.BLUE + ChatColor.UNDERLINE + "http://dev.bukkit.org/bukkit-plugins/scriptexec" + ChatColor.GOLD + "\n/se reload: reload the configuration.\n/se execute <name>: execute a script.");
+				} else {
+					sender.sendMessage(ChatColor.RED + "Sorry, but you do not have permission!");
 				}
 				return true;
-			}else if(args.length==1&&args[0].equals("reload")){
-				if(perm(sender,"ScriptExec.reload")){
+			} else if (args.length == 1 && args[0].equals("reload")) {
+				if (perm(sender, "ScriptExec.reload")) {
 					plugin.reloadConfig();
-					sender.sendMessage(ChatColor.GREEN+"Reloaded!");
-				}else{
-					sender.sendMessage(ChatColor.RED+"Sorry, but you do not have permission!");
+					sender.sendMessage(ChatColor.GREEN + "Reloaded!");
+				} else {
+					sender.sendMessage(ChatColor.RED + "Sorry, but you do not have permission!");
 				}
 				return true;
-			}else if(args.length>1&&args[0].equals("execute")){
-				if(perm(sender,"ScriptExec.execute.*")||perm(sender,"ScriptExec.info"+args[1])){
-					String path=plugin.getConfig().getString("scripts."+args[1]+".path");
-					if(!(path==null)){
-						boolean params=args.length>2;
-						if(params&&!(perm(sender,"ScriptExec.execute.*.params")&&!perm(sender,"ScriptExec.execute."+args[1]+".params"))){
-							sender.sendMessage("You do not have permission to use parameters for the command\n"+plugin.getConfig().getString("scripts."+args[1]+".path"));
+			} else if (args.length > 1 && args[0].equals("execute")) {
+				if (perm(sender, "ScriptExec.execute.*") || perm(sender, "ScriptExec.info" + args[1])) {
+					String path = plugin.getConfig().getString("scripts." + args[1] + ".path");
+					if (!(path == null)) {
+						boolean params = args.length > 2;
+						if (params && (!perm(sender, "ScriptExec.execute.*.params") && !perm(sender, "ScriptExec.execute." + args[1] + ".params"))) {
+							sender.sendMessage("You do not have permission to use parameters for the command\n" + plugin.getConfig().getString("scripts." + args[1] + ".path"));
 							return true;
 						}
 						Runtime r = Runtime.getRuntime();
-						boolean success=true;
-						if(params){
-							for(int i=2;i<=args.length-1;i++){
-								path+=" "+args[i];
-								if(args[i].equals("|")||args[i].equals("&")||args[i].equals("&&")||args[i].equals("||")||args[i].equals(">")||args[i].equals(">>")){
-									sender.sendMessage(ChatColor.RED+"Sorry, but you cannot run multiple commands or output into a file.");
+						boolean success = true;
+						if (params) {
+							for (int i = 2; i <= args.length - 1; i++) {
+								path += " " + args[i];
+								if (args[i].equals("|") || args[i].equals("&") || args[i].equals("&&") || args[i].equals("||") || args[i].equals(">") || args[i].equals(">>")) {
+									sender.sendMessage(ChatColor.RED + "Sorry, but you cannot run multiple commands or output into a file.");
 									return true;
 								}
-								if(args[i].contains(" ")){
-									sender.sendMessage(ChatColor.RED+"Sorry, but you cannot put commands in quotes.");
+								if (args[i].contains(" ")) {
+									sender.sendMessage(ChatColor.RED + "Sorry, but you cannot put commands in quotes.");
 									return true;
 								}
 							}
@@ -57,36 +59,37 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor{
 							r.exec(path);
 						} catch (IOException e) {
 							e.printStackTrace();
-							success=false;
+							success = false;
 						}
-						if(success){
-							sender.sendMessage(ChatColor.GREEN+"Success!");
-						}else{
-							sender.sendMessage(ChatColor.RED+"Uh oh. Something bad happened. Maybe the command/script\n"+plugin.getConfig().getString("scripts."+args[1]+".path")+" does not exist.");
+						if (success) {
+							sender.sendMessage(ChatColor.GREEN + "Success!");
+						} else {
+							sender.sendMessage(ChatColor.RED + "Uh oh. Something bad happened. Maybe the command/script\n" + plugin.getConfig().getString("scripts." + args[1] + ".path") + " does not exist.");
 						}
-					}else{
-						sender.sendMessage(ChatColor.RED+"Uh oh. That script is not found! If you added it, run /scriptexec reload.");
+					} else {
+						sender.sendMessage(ChatColor.RED + "Uh oh. That script is not found! If you added it, run /scriptexec reload.");
 					}
-				}else{
-					sender.sendMessage(ChatColor.RED+"Sorry, but you do not have permission!");
+				} else {
+					sender.sendMessage(ChatColor.RED + "Sorry, but you do not have permission!");
 				}
 				return true;
-			}else{
-				sender.sendMessage(ChatColor.RED+"Sorry, but that is a non-existant command. Available Commands:"+ChatColor.GREEN+"\n/se reload: reload the configuration.\n/se execute <name>: execute a script.");
+			} else {
+				sender.sendMessage(ChatColor.RED + "Sorry, but that is a non-existant command. Available Commands:" + ChatColor.GREEN + "\n/se reload: reload the configuration.\n/se execute <name>: execute a script.");
 				return true;
 			}
 		}
 		return true;
 	}
-	private boolean perm(CommandSender player,String perm){
-		if(!(player instanceof Player)||!ScriptExec.hasPerms){
+
+	private boolean perm(CommandSender player, String perm) {
+		if (!(player instanceof Player) || !ScriptExec.hasPerms) {
 			return true;
 		}
 		Player fplayer = (Player) player;
-		if(ScriptExec.perms.has(fplayer,perm)){
+		if (ScriptExec.perms.has(fplayer, perm)) {
 			return true;
-		}else{
-			if(ScriptExec.perms.has(fplayer,"ScriptExec.*")){
+		} else {
+			if (ScriptExec.perms.has(fplayer, "ScriptExec.*")) {
 				return true;
 			}
 		}
--- src/main/java/com/lights0123/ScriptExec/ScriptExec.java
+++ src/main/java/com/lights0123/ScriptExec/ScriptExec.java
@@ -1,50 +1,47 @@
 package com.lights0123.ScriptExec;
+
 import java.io.IOException;
 
 import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
 public final class ScriptExec extends JavaPlugin{
-	public static boolean hasPerms=false;
-	public static Permission perms;
+	static boolean hasPerms=false;
+	static Permission perms;
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		this.saveDefaultConfig();
 		getConfig().options().copyDefaults(true);
 		saveConfig();
-		if(!setupPermissions()){
-			if(getConfig().getBoolean("require-vault")){
+		if (!setupPermissions()) {
+			if (getConfig().getBoolean("require-vault")) {
 				getLogger().severe("Vault not found. Shutting down...");
 				getServer().getPluginManager().disablePlugin(this);
-	            return;
-			}else{
+				return;
+			} else {
 				getLogger().warning("Vault not found. All players will have permission to execute scripts!");
 			}
-			hasPerms=false;
-		}else{
-			hasPerms=true;
-		}try {
-			Metrics metrics = new Metrics(this);
-	        metrics.start();
-		} catch (IOException e) {
-	    	//Metrics not available :-(
-	    }
+			hasPerms = false;
+		} else {
+			hasPerms = true;
+		}
 		this.getCommand("se").setExecutor(new CommandExecutor(this));
 	}
-	public void onDisable(){
-		String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion());
+
+	public void onDisable() {
+		getLogger().info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
 	}
-    private boolean setupPermissions() {
-    	if (getServer().getPluginManager().getPlugin("Vault")==null) {
-            return false;
-        }
-        RegisteredServiceProvider<Permission> rsp=getServer().getServicesManager().getRegistration(Permission.class);
-        if (rsp == null) {
-            return false;
-        }
-        perms = rsp.getProvider();
-        return perms != null;
-    }
+
+	private boolean setupPermissions() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+		if (rsp == null) {
+			return false;
+		}
+		perms = rsp.getProvider();
+		return perms != null;
+	}
 }
